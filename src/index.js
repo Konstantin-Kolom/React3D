33b1cdad7c3f4255933d65d037c0ecad7f7d3a35
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2023-02-20 20:50:22 +0200

    1

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 
 import 'normalize.css';
 import './index.css';
@@ -11,9 +11,11 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
+  //   <HashRouter>
   <BrowserRouter>
     <App />
   </BrowserRouter>,
+  //   </HashRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
