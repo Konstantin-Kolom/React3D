fe697e75cca8b1057b21955dd698b1c48030b449
Konstantin Kolomiec <77701872+Konstantin-Kolom@users.noreply.github.com>
2023-03-05 14:03:30 +0200

    fix

--- src/components/Text/TextCanvas.js
+++ src/components/Text/TextCanvas.js
@@ -1,5 +1,5 @@
-import { Text, OrbitControls, PerspectiveCamera } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
+import { Text, OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
 
 const TextCanvas = () => {
   return (
@@ -14,19 +14,19 @@ const TextCanvas = () => {
           lineHeight={1.5}
           letterSpacing={0.1}
           textAlign="right"
-          font="https://fonts.gstatic.com/s/cookie/v8/syky-y18lb0tSbf9kgqU.woff"
+          //  font="https://fonts.gstatic.com/s/cookie/v8/syky-y18lb0tSbf9kgqU.woff"
+          //  font="https://fonts.gstatic.com/s/cookie/v8/syky-y18lb0tSbf9kgqU.woff"
           anchorX="center"
           anchorY="middle"
           strokeWidth="3%"
           strokeColor="blue"
         >
           {/* <meshBasicMaterial attach="material" color="green" /> */}
-          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat
-          exercitationem quod minus error beatae tempore non vel deleniti
-          voluptates impedit, aspernatur ducimus magni illo modi debitis dolore.
-          Ipsa cupiditate, molestiae beatae nobis voluptates ad quae maiores.
-          Eligendi repellat quibusdam perferendis consequuntur ut libero
-          accusantium, suscipit pariatur iusto molestiae, labore impedit!"
+          "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quaerat exercitationem quod
+          minus error beatae tempore non vel deleniti voluptates impedit, aspernatur ducimus magni
+          illo modi debitis dolore. Ipsa cupiditate, molestiae beatae nobis voluptates ad quae
+          maiores. Eligendi repellat quibusdam perferendis consequuntur ut libero accusantium,
+          suscipit pariatur iusto molestiae, labore impedit!"
         </Text>
       </Canvas>
     </>
